6afa4723b62d262c3a893012cd5a7e23183e7b76
PerfectCode01 <japhetbaz04@gmail.com>
2026-06-22 11:57:57 +0100

    Fix: force base path to root for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // ➔ AJOUTE CETTE LIGNE ICI
   plugins: [
     react(), 
     tailwindcss()
